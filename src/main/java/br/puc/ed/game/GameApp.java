17116ae4181ebb62a2f885a71f9d46465a619866
Humberto Ferreira da Luz Junior <hfluz@uel.br>
2021-03-05 18:27:35 -0300

    Preenchimento padrão do painel com os mesmos dados dos slides.

--- src/main/java/br/puc/ed/game/GameApp.java
+++ src/main/java/br/puc/ed/game/GameApp.java
@@ -4,7 +4,16 @@ public class GameApp {
 
   public static void main(String[] args) {
     PainelPontuacao painelPontuacao = new PainelPontuacao(5);
-    painelPontuacao.adicionar(new RegistroPontuacao("João", 4));
+    painelPontuacao.adicionar(new RegistroPontuacao("Lúcia", 57));
+    painelPontuacao.adicionar(new RegistroPontuacao("Ricardo", 79));
+    painelPontuacao.adicionar(new RegistroPontuacao("Maria", 42));
+    painelPontuacao.adicionar(new RegistroPontuacao("Erick", 50));
+    painelPontuacao.imprimir();
+    painelPontuacao.adicionar(new RegistroPontuacao("Vinícius", 55));
+    painelPontuacao.imprimir();
+    painelPontuacao.adicionar(new RegistroPontuacao("João", 43));
+    painelPontuacao.imprimir();
+    painelPontuacao.adicionar(new RegistroPontuacao("Rafael", 35));
     painelPontuacao.imprimir();
   }
 }
